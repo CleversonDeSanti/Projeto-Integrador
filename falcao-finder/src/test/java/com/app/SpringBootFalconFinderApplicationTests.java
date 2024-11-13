4f54966989994f2d5e2d039b91404cdd7457fdc0
ocleverson <cleverson.santi@unochapeco.edu.br>
2024-11-13 20:13:28 -0300

    Commit Refactor

--- falcao-finder/src/test/java/com/app/SpringBootFalconFinderApplicationTests.java
+++ falcao-finder/src/test/java/com/app/SpringBootFalconFinderApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootCrudWithThymeleafApplicationTests {
+public class SpringBootFalconFinderApplicationTests {
 
 	@Test
 	public void contextLoads() {
